786658492f40b375d2b68b665c98c35a37a5b79d
David Fleming <tonage72@gmail.com>
2024-04-30 18:49:32 -0400

    Getting there with JS

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,7 @@
+const inputFirstName = document.getElementsByName("input-first-name")[0];
+const inputLastName = document.getElementsByName("input-last-name")[0];
+const inputEmail = document.getElementsByName("input-email")[0];
+const inputPassword = document.getElementsByName("input-password")[0];
 const buttonClaim = document.querySelector(".button-claim");
 
 buttonClaim.addEventListener('click', e => {
@@ -5,12 +9,31 @@ buttonClaim.addEventListener('click', e => {
 })
 
 function validateInputs() {
-	const inputFirstName = document.getElementsByName("input-first-name")[0].value.trim();
-	const inputLastName = document.getElementsByName("input-last-name")[0].value.trim();
-	const inputEmail = document.getElementsByName("input-email")[0].value.trim();
-	const inputPassword = document.getElementsByName("input-password")[0].value.trim();
+	const inputFirstNameValue = document.getElementsByName("input-first-name")[0].value.trim();
+	const inputLastNameValue = document.getElementsByName("input-last-name")[0].value.trim();
+	const inputEmailValue = document.getElementsByName("input-email")[0].value.trim();
+	const inputPasswordValue = document.getElementsByName("input-password")[0].value.trim();
 
-	if(inputFirstName === '') {
-		displayError()
+	if(inputFirstNameValue === '') {
+		displayError(inputFirstName, 'First name can\'t be blank.')
 	}
+
+	if(inputLastNameValue === '') {
+		displayError(inputLastName, 'Last name can\'t be blank.')
+	}
+
+	if(inputEmailValue === '') {
+		displayError(inputEmail, 'Email can\'t be blank.')
+	}
+
+	if(inputPasswordValue === '') {
+		displayError(inputPassword, 'Password can\'t be blank.')
+	}
+}
+
+function displayError(input, message) {
+	const inputFrame = input.parentElement;
+	const small = inputFrame.querySelector('small');
+	inputFrame.className = 'input-frame error'
+	small.innerText = message;
 }
